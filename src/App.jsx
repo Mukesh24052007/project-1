2309adec05508bed506f3627dcd6ebfee4f26c34
Mukesh24052007 <mukeshveralevel@gmail.com>
2025-07-19 19:00:36 +0530

    updates icon and title.

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,7 @@ const App = () => {
         <AppBar position="static" sx={{ background: 'transparent', boxShadow: 'none' }}>
           <Toolbar>
             <img
-              src="/logo.jpg"
+              src="/logo.png"
               alt="Your Brand Logo"
               style={{ height: '50px', marginRight: '16px' }}
             />
